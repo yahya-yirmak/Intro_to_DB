c548cfa902e7d3a6f26d67a47d7b3d3ddb504474
yahya yirmak <yahyayirmak9@gmail.com>
2024-10-20 17:43:17 +0100

    Updating alx_book_store database script

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create Order_Details database table :
