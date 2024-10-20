7f72b8ac1e70cefb7df19b5f3e63ad0f777a8b3a
yahya yirmak <yahyayirmak9@gmail.com>
2024-10-20 18:42:09 +0100

    Adding task_5 database script

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+
+-- Insert a single row into the customers table
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
