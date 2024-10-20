1911af3f49d4770ab8e6e407fa403efc3175c88c
yahya yirmak <yahyayirmak9@gmail.com>
2024-10-20 17:43:54 +0100

    Updating alx_book_store database script

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -43,6 +43,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
