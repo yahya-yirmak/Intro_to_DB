d57a7d8defd45b7cab9a6a016c0005c2c5412b44
yahya yirmak <yahyayirmak9@gmail.com>
2024-10-20 18:40:21 +0100

    Updating task_4 database script

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 
--- Retrieve the full description of the books table
--- Retrieve the full description of the books table
+-- Retrieve the full description of the Books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+AND TABLE_NAME = 'Books';
+
