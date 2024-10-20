be894bb7f24127c41b8920dcfa740bc84fa455bf
yahya yirmak <yahyayirmak9@gmail.com>
2024-10-20 19:49:58 +0100

    Updating task_6 database script

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,6 @@
 -- Insert multiple rows into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'), (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'), (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
